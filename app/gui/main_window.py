3ef75b52b56eccca9981d7c4aaa2e1c27c5b20de
Melker Widén <melker.widen@gmail.com>
2024-05-30 00:59:29 +0200

    Add Njord version to window title

--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -1,13 +1,14 @@
 from PySide6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QVBoxLayout
 
 from .compund_widgets import CallForm, GenerateReportBar, CallTable
+from app.configuration.global_config import get_version
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Njord")
+        self.setWindowTitle(f"Njord {get_version()}")
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
